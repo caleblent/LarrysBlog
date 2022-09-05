c569fd55305ea866d5692492dce810a84a5b0c91
= <caleb@stcroixwebagency.com>
2022-09-05 13:19:18 -0500

    unsure what is

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,7 +8,7 @@
     <div class="mt-8 md:mt-0">
         <a href="/" class="text-xs font-bold uppercase">Home Page</a>
 
-        <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+        <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
             Subscribe for Updates
         </a>
     </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -23,25 +23,4 @@
 
 </main>
 
-    <!-- <h1>Larry's Blog</h1>
-
-    @foreach($posts as $post)
-
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-
-        <p>
-            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-        </p>
-
-        <div>
-            {{ $post->excerpt }}
-        </div>
-    </article>
-
-    @endforeach -->
 @endsection
